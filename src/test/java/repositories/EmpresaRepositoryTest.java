136febceac0716652a9ad96536d5e23ba47f3097
thiagojmuller <thiago.j.muller@hotmail.com>
2017-10-14 17:29:30 -0300

    Adicionado novo properties para test

---
+++ src/test/java/repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,24 @@
+package repositories;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class EmpresaRepositoryTest {
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	private static final String CNPJ = "51463645000100";
+	
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+	
+}
